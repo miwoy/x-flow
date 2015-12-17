9c72469bd8be1325bc29c0b645890fd199a46528
ZXL <zhouxiuliang@suishipen.com>
2015-12-17 11:45:13 +0800

    test

--- test.js
+++ test.js
@@ -25,6 +25,7 @@ x.begin()
             self.step1 = true;
             self.err();
             //self.next();
+            console.log("");
         });
     })
     // 用回调函数获取上一次异步结果，并返回一个x流执行数据结构的数组对象
